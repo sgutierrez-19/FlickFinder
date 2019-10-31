285c7adf203a14f5e76191855b551c1b1c12bf91
jjestrella <estrella.jjuan@gmail.com>
2019-10-31 13:45:24 -0700

    update movie.js file

---
+++ server/models/movie.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+  var Movie = sequelize.define("Movie", {
+    movie_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [10]
+      }
+    },
+    movie_year: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    overview: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    poster_path: {
+      type: type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+
+  Movie.associate = function (models) {
+    Movie.belongsTo(models.User);
+  }
+  
+  return Movie;
+};
