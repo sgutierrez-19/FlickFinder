8c78e6dba8082aa02a183918c1ea05e86b7166b3
Steven Gutierrez <stematgut@gmail.com>
2019-11-02 16:59:40 -0700

    was able to get favorite button to work on the movies page - fianlly

--- client/public/js/movies-ajax.js
+++ client/public/js/movies-ajax.js
@@ -3,62 +3,57 @@ var apikey = "539ccda6c942a1dfd00efc7df43be0d1"
 
 
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     // these will be buttons most likely
     var getRecommended = $(".rec-movies");
     var searchMovies = $("#movie-search");
     var nowPlaying = $(".now-playing");
     // search box for movies
     var movieInput = $("#movie-search-title");
-    
+
 
     // ajax for searching a specific movie that the user can then vote on
-    searchMovies.on("submit", function(event){
-        
+    searchMovies.on("submit", function (event) {
+
         var movieInputVal = movieInput.val();
         event.preventDefault();
         var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=" + apikey + "&language=en-US&query=" + movieInputVal + "&page=1";
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(results){
+        }).then(function (results) {
             console.log(results);
             // do stuff with the results here (fill divs add info elsewhere etc)
-        }).catch(function(err){
+        }).catch(function (err) {
             if (err) throw err;
             console.log(err);
         })
     })
-    
+
     // NOW PLAYING AJAX
     // when we click the button to search for the current playing movies
     // if it just shows now playing when we load the page, we'll get rid of the on click method
-    nowPlaying.on("click", function(event){
+    nowPlaying.on("click", function (event) {
         event.preventDefault();
         // building the url for the ajax
         var queryURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + apikey + "&language=en-US&page=1";
-        
+
         // ajax for now playing movies
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(result){
+        }).then(function (result) {
             console.log(result);
             // do shit with the data we get back
             // prob store data in our database here using associations?
 
-            for (var i = 0; i<5; i++){
+            for (var i = 0; i < 5; i++) {
                 var movieTitle = result.results[i].title;
                 var movieId = result.results[i].id;
                 var moviePlot = result.results[i].overview;
                 var moviePoster = result.results[i].poster_path;
-                
-                
-                
-                
-               
-                
+
                 var npDiv = $("<div>");
                 npDiv.attr("class", "card");
                 // img div
@@ -75,61 +70,33 @@ $(document).ready(function(){
                 npFooterTxt.text(movieTitle);
                 npFooter.append(npFooterTxt);
                 npDiv.append(npFooter);
-               
-                // npDiv.attr("data-title", movieTitle);
-                // npDiv.attr("data-id", movieId);
-                // npDiv.attr("data-poster", moviePoster);
-                
-            
-                // var txtDiv = $("<div>");
-                // var h1Title = $("<h1>");
-                // var pPlot = $("<p>");
-
-                // h1Title.text("Title: " + movieTitle);
-                // pPlot.text("Plot: " + moviePlot);
-                
-               
-                
-                
-                // txtDiv.append(h1Title);
-                // txtDiv.append(pPlot);
-                // txtDiv.append(npImg);
-                
-                
-                // npDiv.append(txtDiv);
-                
 
                 $("#npContainer").append(npDiv);
-                
-                
             }
-       
-       
-        }).catch(function(err){
+        }).catch(function (err) {
             if (err) throw err;
             console.log(err);
         });
-
-
     });
+
     // recommended movie button ajax
-    getRecommended.on("click", function(event){
+    getRecommended.on("click", function (event) {
         // movieId is from the searched movie 
         var movieId;
-        
+
         event.preventDefault();
-        var queryURL = "https://api.themoviedb.org/3/movie/" + movieId + "/recommendations?api_key=" + apikey+ "&language=en-US&page=1"
+        var queryURL = "https://api.themoviedb.org/3/movie/" + movieId + "/recommendations?api_key=" + apikey + "&language=en-US&page=1"
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(results){
+        }).then(function (results) {
             console.log(results);
             // do more shit with the results from this ajax
-        }).catch(function(err){
+        }).catch(function (err) {
             if (err) throw err;
             console.log(err);
         })
-    
+
     })
 
 
--- server/models/movie.js
+++ server/models/movie.js
@@ -1,14 +1,11 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var Movie = sequelize.define("Movie", {
     movie_title: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        len: [10]
-      }
     },
     movie_year: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: false
     },
     overview: {
@@ -18,12 +15,24 @@ module.exports = function(sequelize, DataTypes) {
     poster_path: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    favorited: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    }, 
+    createdAt: {
+      allowNull: true,
+      type: DataTypes.DATE
+    }, 
+    updatedAt: {
+      allowNull: true,
+      type: DataTypes.DATE
     }
-  });
+  } );
 
   Movie.associate = function (models) {
     Movie.belongsTo(models.User);
   }
-  
+
   return Movie;
 };
